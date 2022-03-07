13c43ff6b20d3aa26db5d596152367b37f14431c
sebastian.ukleja <sebastian.ukleja@fh-campuswien.ac.at>
2022-03-07 12:43:33 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+
+
+x:int = 42  # this comment
+y = 4.42
+text = "Test"
+text2 = "Another Test"
+bool1 = False
+
+z = str(x)
+print(type(z))
+
+result = 10
+
+def sum_numbers(num1, num2):
+    result = num1 + num2
+    return result
+
+
+sum_numbers(x,y)
+print(result)
+
+def check_number(number):
+    if number < 10 and number > 2:
+        return number
+
+def loop_test(number):
+    for i in range(number):
+        if i != 3:
+            print(i, end = " ")
+        else:
+            continue
+
+loop_test(6)
+
